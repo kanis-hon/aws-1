39c5a8676aef53523ba717ba07864b1f9c15cd6b
Kanisorn Hongwatcharasakun <kanisorn.hongwatchar@thomsonreuters.com>
2014-11-29 00:41:09 +0700

    (feature) include reading property file
    install.sh
    + read property file
    + ssh execute commands to the target server and then close connection

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+set -e
 
-ssh -i $certificate_path  $user@$ip -t 'xxxxx; bash -l'
+PROPERTY_FILE=./config/vm.props
+SSH=`which ssh`
+
+echo "----------------------------------------------------------------"
+echo " INCLUDE PROPERTY FILE "
+echo "----------------------------------------------------------------"
+
+echo "FILE: $PROPERTY_FILE"
+. $PROPERTY_FILE
+
+
+echo "----------------------------------------------------------------"
+echo " CONNECT VM "
+echo "----------------------------------------------------------------"
+
+echo "IP: $IP"
+echo "CERTIFICATE: $CERTIFICATE_PATH/$CERTIFICATE_NAME"
+echo "USER: $USER"
+$SSH -i $CERTIFICATE_PATH/$CERTIFICATE_NAME  $USER@$IP -t \
+        'mkdir /tmp/vm; ' \
+        'exit; bash -l'
 
