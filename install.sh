b3cbf8901404ded08578f2b8b1f12277ed02c737
Kanisorn Hongwatcharasakun <kanisorn.hongwatchar@thomsonreuters.com>
2014-11-29 03:44:14 +0700

    (feature) checkout code from github repo
    - git pull puppet-jenkins module

--- install.sh
+++ install.sh
@@ -24,10 +24,34 @@ echo "USER: $USER"
 $CHMOD $CERTIFICATE_PERMISSION $CERTIFICATE_PATH/$CERTIFICATE_NAME
 
 $SSH -i $CERTIFICATE_PATH/$CERTIFICATE_NAME  $USER@$IP -t \
-		'echo ----------------------------------------------------------------; ' \
-		'echo INSTALL PUPPET; ' \
-		'echo ----------------------------------------------------------------; ' \
-        'sudo apt-get install -y puppet; ' \
-        'exit; bash -l'
+	'echo ----------------------------------------------------------------; ' \
+	'echo UPDATE APT-GET repo; ' \
+	'echo ----------------------------------------------------------------; ' \
+	'sudo apt-get update; ' \
+	'echo ;' \
+	'echo ----------------------------------------------------------------; ' \
+	'echo INSTALL PUPPET; ' \
+	'echo ----------------------------------------------------------------; ' \
+    'sudo apt-get install -y puppet; ' \
+	'echo ;' \
+	'echo ----------------------------------------------------------------; ' \
+	'echo INSTALL GIT; ' \
+	'echo ----------------------------------------------------------------; ' \
+    'sudo sudo apt-get install -y git; ' \
+	'echo ;' \
+    'echo ----------------------------------------------------------------; ' \
+	'echo CREATE WORKSPACE; ' \
+	'echo ----------------------------------------------------------------; ' \
+	'echo PATH: /workspace/puppet/modules/puppet-jenkins/; ' \
+    'sudo mkdir -p /workspace/puppet/modules/puppet-jenkins/ ;' \
+	'echo ;' \
+    'echo ----------------------------------------------------------------; ' \
+	'echo CHECKOUT GIT; ' \
+	'echo ----------------------------------------------------------------; ' \
+	'cd /workspace/puppet/modules/puppet-jenkins/ ;' \
+	'sudo git init ;' \
+	"sudo git pull $GIT_REPO_URL ;" \
+	'echo PATH: /workspace/puppet/modules/puppet-jenkins/; ' \
+    'exit; bash -l'
 
 #sudo apt-get autoremove puppet
