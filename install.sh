3ffca6390e43b9490e89b17e97dfb66d71a5d850
Kanisorn Hongwatcharasakun <kanisorn.hongwatchar@thomsonreuters.com>
2014-12-07 02:34:03 +0700

    (feature) add EC2-CLI support for install script

--- install.sh
+++ install.sh
@@ -1,57 +1,50 @@
 #!/bin/bash
 set -e
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 PROPERTY_FILE=./config/vm.props
-SSH=`which ssh`
-CHMOD=`which chmod`
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+COLOR='\033[0m'
+DOWNLOAD_DIR='./download'
+EC2_API_ZIP='ec2-api-tools.zip'
+EC2_API_DOWNLOAD_URL="http://s3.amazonaws.com/ec2-downloads"
+EC2_API_HOME='/usr/local/ec2'
 
+echo "================================================================"
+echo "NOTE: THIS SCRIPT IS SUPPORT ON MAC OSX..."
+echo "================================================================"
+echo
+echo "INCLUDE PROPERTY FILE"
 echo "----------------------------------------------------------------"
-echo " INCLUDE PROPERTY FILE "
-echo "----------------------------------------------------------------"
-
-echo "FILE: $PROPERTY_FILE"
 . $PROPERTY_FILE
+echo "  FILE(S):"
+echo -e "    ${COLOR}- $PROPERTY_FILE\t\t\t\t\t[OK]"
 
+echo
+echo "INSTALL EC2 API TOOLS"
+echo "----------------------------------------------------------------"
+wget "$EC2_API_DOWNLOAD_URL/$EC2_API_ZIP" -P $DOWNLOAD_DIR
+#mkdir /usr/local/ec2
+unzip "$DOWNLOAD_DIR/$EC2_API_ZIP" -d $EC2_API_HOME
 
 echo "----------------------------------------------------------------"
-echo " CONNECT VM "
+echo "CONNECT VM "
 echo "----------------------------------------------------------------"
 
-echo "IP: $IP"
-echo "CERTIFICATE: $CERTIFICATE_PATH/$CERTIFICATE_NAME"
-echo "USER: $USER"
+echo "  IP: $IP"
+echo "  CERTIFICATE: $CERTIFICATE_PATH/$CERTIFICATE_NAME"
+echo "  USER: $USER"
 
 $CHMOD $CERTIFICATE_PERMISSION $CERTIFICATE_PATH/$CERTIFICATE_NAME
 
-$SSH -i $CERTIFICATE_PATH/$CERTIFICATE_NAME  $USER@$IP -t \
-	'echo ----------------------------------------------------------------; ' \
-	'echo UPDATE APT-GET repo; ' \
-	'echo ----------------------------------------------------------------; ' \
-	'sudo apt-get update; ' \
-	'echo ;' \
-	'echo ----------------------------------------------------------------; ' \
-	'echo INSTALL PUPPET; ' \
-	'echo ----------------------------------------------------------------; ' \
-    'sudo apt-get install -y puppet; ' \
-	'echo ;' \
-	'echo ----------------------------------------------------------------; ' \
-	'echo INSTALL GIT; ' \
-	'echo ----------------------------------------------------------------; ' \
-    'sudo sudo apt-get install -y git; ' \
-	'echo ;' \
-    'echo ----------------------------------------------------------------; ' \
-	'echo CREATE WORKSPACE; ' \
-	'echo ----------------------------------------------------------------; ' \
-	'echo PATH: /workspace/puppet/modules/puppet-jenkins/; ' \
-    'sudo mkdir -p /workspace/puppet/modules/puppet-jenkins/ ;' \
-	'echo ;' \
-    'echo ----------------------------------------------------------------; ' \
-	'echo CHECKOUT GIT; ' \
-	'echo ----------------------------------------------------------------; ' \
-	'cd /workspace/puppet/modules/puppet-jenkins/ ;' \
-	'sudo git init ;' \
-	"sudo git pull $GIT_REPO_URL ;" \
-	'echo PATH: /workspace/puppet/modules/puppet-jenkins/; ' \
-    'exit; bash -l'
+$SSH -i $CERTIFICATE_PATH/$CERTIFICATE_NAME  $USER@$IP 'bash -s' -- < ./deploy_puppet.sh "$GIT_REPO_URL"
+#  ./deploy_puppet.sh
+#EOF
+
 
 #sudo apt-get autoremove puppet
